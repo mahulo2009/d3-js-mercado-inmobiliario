5799a9a9eac4e90e43f60a547f3a6c71c0d50705
mhuertas <manuel.huertas.lopez@gmail.com>
2023-05-04 20:08:11 +0000

    First version

--- src/actividadGrupal.js
+++ src/actividadGrupal.js
@@ -1,44 +1,94 @@
 console.log ("Hello World")
 
-d3.json ("https://gist.githubusercontent.com/double-thinker/817b155fd4fa5fc865f7b32007bd8744/raw/13068b32f82cc690fb352f405c69c156529ca464/partidos2.json").then (function (datosCompletos){
-    
-    
-    // NUEVA VARIABLE CON LA INFO DE PARTIDOS DEL NUEVO DATASET
-    var datos = datosCompletos.partidos
-    //En nuestro caso será:
-    //var datos = datosCompletos.Metricas
-    
-    
-    
-    console.log ("los datos ya se han recibido")
-    
-    var height = 600
-    var width = 400
-    
-    var margin  = {
-        top: 20,
-        botton: 50,
-        left:40,
-        right:50
-            
-    }
-    
-    //var elementosvg=d3.select ("#grafica1")
-    var svgGraficaBarras=d3.select ("body")
+
+d3.json("https://raw.githubusercontent.com/mahulo2009/d3-js-mercado-inmobiliario/main/data/indicadores_del_mercado_inmobiliario.json").then(function (datosCompletos) {
+
+    console.log("Los datos ya se han recibido v1")
+
+    var datos = datosCompletos.Respuesta.Datos.Metricas[0].Datos
+
+    var meses = {
+        "Enero": 1,
+        "Febrero": 2,
+        "Marzo": 3,
+        "Abril": 4,
+        "Mayo": 5,
+        "Junio": 6,
+        "Julio": 7,
+        "Agosto": 8,
+        "Septiembre": 9,
+        "Octubre": 10,
+        "Noviembre": 11,
+        "Diciembre": 12
+    };
+
+    datos.forEach(function (d) {
+        var fecha = d.Agno + "-" + (meses[d.Periodo] < 10 ? "0" : "") + meses[d.Periodo] + "-01";
+        d.fecha = fecha;
+
+    });
+
+    var ancho = 800;
+    var altura = 500;
+    var margen = { superior: 50, derecho: 20, inferior: 30, izquierdo: 50 };
+
+    var svg = d3.select("body")
         .append("svg")
-        .attr("width",width)
-        .attr("height",height)
-    
-    
-    
-    
-    
-    // var svgHistograma=d3.select ("#grafica2")
-    var svgGraficaLineas=d3.select ("body")
-        .append("svg")     
-        .attr("width",width)
-        .attr("height",height)
-    
-    
-    
+        .attr("width", ancho + margen.izquierdo + margen.derecho)
+        .attr("height", altura + margen.superior + margen.inferior)
+        .append("g")
+        .attr("transform", "translate(" + margen.izquierdo + "," + margen.superior + ")");
+
+
+    var escalaX = d3.scaleTime()
+        .domain(d3.extent(datos, function (d) { return new Date(d.fecha); }))
+        .range([0, ancho]);
+
+    var escalaY = d3.scaleLinear()
+        .domain([d3.min(datos, function (d) { return d.Valor; }) - 20, d3.max(datos, function (d) { return d.Valor; }) + 20])
+        .range([altura, 0]);
+
+    var ejeX = d3.axisBottom(escalaX);
+    var ejeY = d3.axisLeft(escalaY);
+
+    // Title
+    svg.append('text')
+        .attr('x', ancho / 2)
+        .attr('y', 0)
+        .attr('text-anchor', 'middle')
+        .style('font-family', 'Helvetica')
+        .style('font-size', 20)
+        .text("Indicadores del mercado inmobiliario");
+
+    svg.append('text')
+        .attr('x', ancho / 2)
+        .attr('y', 25)
+        .attr('text-anchor', 'middle')
+        .style('font-family', 'Helvetica')
+        .style('font-size', 20)
+        .text("Porcentaje de variacion vivienda nuevas");
+
+
+    svg.append("g")
+        .attr("transform", "translate(0," + altura + ")")
+        .call(ejeX);
+
+    svg.append("g")
+        .call(ejeY);
+
+    var linea = d3.line()
+        .x(function (d) { return escalaX(new Date(d.fecha)); })
+        .y(function (d) { return escalaY(d.Valor); })
+        .curve(d3.curveMonotoneX)
+
+    svg.append("path")
+        .datum(datos)
+        .attr("d", linea)
+        .attr("stroke", "steelblue")
+        .attr("fill", "none")
+        .style("stroke-width", "2");
+
+
+
+
 })
